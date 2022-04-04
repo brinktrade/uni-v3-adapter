a5dd605b1ce25d81a31aad2525f7d7a4e1ccc72c
Mike Calvanese <mfcalvanese@gmail.com>
2022-04-04 14:28:13 -0400

    add UniV3Adapter02 with min arb params in uniV3Swap function

--- __snapshots__/deterministicAddress.test.js
+++ __snapshots__/deterministicAddress.test.js
@@ -1,3 +1,7 @@
 exports['UniV3Adapter.sol deterministic address check 1'] = `
 0xC938077C5334Ab2969eCCF6319A73ab363bA448e
 `
+
+exports['UniV3Adapter02.sol deterministic address check 1'] = `
+0xBaF92C69354B6D92ee9f6eE6022E4BB13A7c4590
+`
